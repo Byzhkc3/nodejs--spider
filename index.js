37e8cca32cde62b96219173b3ea0f521efd0b637
aatii <sdgjytu@qq.com>
2016-10-17 16:55:37 -0400

    test

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-var superagent = require("superagent");
 var request = require("request");
 var cheerio = require("cheerio");
+
+request('https://www.shanbay.com/wordbook/23/', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var $ = cheerio.load(body);
+        console.log($('.wordbook-wordlist-name').text());
+    }
+});
