15f8cdd3a2ff0d630671790f2824beff26f80518
aatii <sdgjytu@qq.com>
2016-10-23 23:55:04 -0400

    spider

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 var db = require("./db");
-var cookies = require("./cookies");
 var tools = require("./tools");
 var config = require('./config');
+var superagent = require("superagent");
+var cheerio = require('cheerio');
 
-var cookie;
+var usercookie = null;
 
 function main() {
-    tools.post(config.url, )
+    cookies.getCookie
+}
+
+function getloginCookie() {
+    superagent.post()
+}
+
+function getCsrf(callback) {
+    tools.get(config.url, usercookie.cookie, function (err, data) {
+        if (err) {
+            console.log('Get cookie error');
+            return;
+        }
+        var $ = cheerio(data, { decodeEntities: false });
+        var csrf = $("input[name='csrfmiddlewaretoken']").val();
+        if (!csrf) {
+            console.log('csrfmiddlewaretoken not found');
+            return;
+        }
+        return csrf;
+    });
 }
--- tools.js
+++ tools.js
@@ -1,21 +1,31 @@
 var request = require("request");
 var zlib = require("zlib");
 
+var headersGet = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, sdch, br',
+    'Accept-Language': 'zh-CN,zh;q=0.8',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Cookie': cookie,
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/53.0.2785.143 Chrome/53.0.2785.143 Safari/537.36'
+};
+var headersPost = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.8',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Cookie': cookie,
+    'Upgrade-Insecure-Requests': 1,
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/53.0.2785.143 Chrome/53.0.2785.143 Safari/537.36'
+}
+
 exports.get = function (url, cookie, callback) {
     request.get({
         url: url,
-        headers: {
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            'Accept-Encoding': 'gzip, deflate, sdch, br',
-            'Accept-Language': 'zh-CN,zh;q=0.8',
-            'Cache-Control': 'max-age=0',
-            'Connection': 'keep-alive',
-            'Cookie': cookie,
-            'Host': 'www.shanbay.com',
-            'Referer': 'https://www.shanbay.com/wordbook/books/mine/',
-            'Upgrade-Insecure-Requests': 1,
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/53.0.2785.143 Chrome/53.0.2785.143 Safari/537.36'
-        },
+        headers: headersGet,
         timeout: 30000,
         encoding: null
     }, function (err, response, data) {
@@ -42,20 +52,7 @@ exports.get = function (url, cookie, callback) {
 exports.post = function (url, cookie, body, callback) {
     request.post({
         url: url,
-        headers: {
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'zh-CN,zh;q=0.8',
-            'Cache-Control': 'max-age=0',
-            'Connection': 'keep-alive',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Cookie': cookie,
-            'Host': 'www.shanbay.com',
-            'Origin': 'https://www.shanbay.com',
-            'Referer': 'https://www.shanbay.com/accounts/login/',
-            'Upgrade-Insecure-Requests': 1,
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/53.0.2785.143 Chrome/53.0.2785.143 Safari/537.36'
-        },
+        headers: headersPost,
         timeout: 30000,
         encoding: null,
         body: body
